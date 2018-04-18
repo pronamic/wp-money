4ee0f9e06247abf528a75cd8bb79d40efea219f2
Remco Tolsma <info@remcotolsma.nl>
2018-04-18 15:42:47 +0200

    Work in progress.

--- src/Currency.php
+++ src/Currency.php
@@ -11,11 +11,52 @@
 namespace Pronamic\WordPress\Money;
 
 /**
- * Money
+ * Currency
  *
  * @author Remco Tolsma
  * @version 1.0
  */
 class Currency {
+	/**
+	 * Code.
+	 *
+	 * @var string
+	 */
+	private $code;
 
+	/**
+	 * Symbol.
+	 *
+	 * @var string
+	 */
+	private $symbol;
+
+	/**
+	 * Name.
+	 *
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * Number decimals.
+	 *
+	 * @var int
+	 */
+	private $number_decimals;
+
+	/**
+	 * Construct and initialize currency object.
+	 *
+	 * @var string $code
+	 * @var string $symbol
+	 * @var string $name
+	 * @var int    $number_decimals
+	 */
+	public function __construct( $code, $symbol, $name, $number_decimals = 2 ) {
+		$this->code            = $code;
+		$this->symbol          = $symbol;
+		$this->name            = $name;
+		$this->number_decimals = $number_decimals;
+	}
 }
--- src/Money.php
+++ src/Money.php
@@ -17,5 +17,46 @@ namespace Pronamic\WordPress\Money;
  * @version 1.0
  */
 class Money {
+	/**
+	 * Amount.
+	 *
+	 * @var float
+	 */
+	private $amount;
 
+	/**
+	 * Currency.
+	 *
+	 * @var Currency
+	 */
+	private $currency;
+
+	/**
+	 * Construct and initialize money object.
+	 *
+	 * @var float         $amount
+	 * @var Currency|null $currency
+	 */
+	public function __construct( $amount = 0, $currency = null ) {
+		$this->amount   = $amount;
+		$this->currency = $currency;
+	}
+
+	/**
+	 * Format i18n.
+	 *
+	 * @return string
+	 */
+	public function format_i18n() {
+		if ( isset( $this->currency ) ) {
+			return sprintf(
+				_x( '%1$s%2$s %3$s', 'money format', 'pronamic-money' ),
+				$this->currency->get_symbol(),
+				number_format_i18n( $this->amount, $this->currency->get_number_decimals() ),
+				$this->currency->get_code(),
+			);
+		}
+
+		return number_format_i18n( $this->amount, 2 );
+	}
 }
