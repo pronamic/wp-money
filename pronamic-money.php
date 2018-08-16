48e5fcc70bb4299cc3487df55e3ef31c18e77ffb
Remco Tolsma <info@remcotolsma.nl>
2018-08-16 11:56:24 +0200

    Updated plugin version number.

--- pronamic-money.php
+++ pronamic-money.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.pronamic.eu/plugins/pronamic-money/
  * Description: WordPress Money library.
  *
- * Version: 1.0.0
+ * Version: 1.1.0
  * Requires at least: 4.7
  *
  * Author: Pronamic
