385ea54fb01a928bf6bd117d29828f289a770c06
Reüel van der Steege <reuel@akwaabamultimedia.nl>
2018-11-09 13:40:30 +0100

    Calculate tax amount or tax percentage in taxed money constructor.

--- src/TaxedMoney.php
+++ src/TaxedMoney.php
@@ -35,13 +35,26 @@ class TaxedMoney extends Money {
 	/**
 	 * Construct and initialize money object.
 	 *
-	 * @param string|int|float     $amount   Amount.
-	 * @param Currency|string|null $currency Currency.
+	 * @param string|int|float     $amount         Amount.
+	 * @param Currency|string|null $currency       Currency.
+	 * @param string|int|float     $tax_amount     Tax amount.
+	 * @param string|int|float     $tax_percentage Tax percentage.
 	 */
 	public function __construct( $amount = 0, $currency = null, $tax_amount = null, $tax_percentage = null ) {
 		parent::__construct( $amount, $currency );
 
+		// Calculate tax amount if tax percentage is set.
+		if ( null === $tax_amount && null !== $tax_percentage ) {
+			$tax_amount = $amount * ( $tax_percentage / 100 );
+		}
+
 		$this->set_tax_amount( $tax_amount );
+
+		// Calculate tax percentage if tax amount is set.
+		if ( null === $tax_percentage && null !== $tax_amount && $amount > 0 ) {
+			$tax_percentage = round( ( $tax_amount / $amount ) * 100, 2 );
+		}
+
 		$this->set_tax_percentage( $tax_percentage );
 	}
 
@@ -75,7 +88,12 @@ class TaxedMoney extends Money {
 	/**
 	 * Set tax percentage.
 	 *
-	 * @param mixed $tax_percentage Tax percentage.
+	 * 100% = 100
+	 *  21% =  21
+	 *   6% =   6
+	 * 1.5% =   1.5
+	 *
+	 * @param string|int|float|null $percentage Tax percentage.
 	 */
 	public function set_tax_percentage( $percentage ) {
 		$this->tax_percentage = floatval( $percentage );
