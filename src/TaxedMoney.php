35d7b68569e9b3be13071cd5dae9c9b90349415e
Reüel van der Steege <reuel@pronamic.nl>
2018-11-14 16:34:53 +0100

    Remove tax percentage calculation.

--- src/TaxedMoney.php
+++ src/TaxedMoney.php
@@ -49,12 +49,6 @@ class TaxedMoney extends Money {
 		}
 
 		$this->set_tax_amount( $tax_amount );
-
-		// Calculate tax percentage if tax amount is set.
-		if ( null === $tax_percentage && null !== $tax_amount ) {
-			$tax_percentage = round( ( $tax_amount / ( $amount - $tax_amount ) ) * 100, 2 );
-		}
-
 		$this->set_tax_percentage( $tax_percentage );
 	}
 
