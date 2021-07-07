fd2dbcdb362eb706cc2e9a817f2e0f7ae505c863
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2021-07-07 11:33:32 +0200

    Add Euro test.

---
+++ tests/src/CurrencyTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Currency Test
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2021 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Money
+ */
+
+namespace Pronamic\WordPress\Money;
+
+use WP_Locale;
+use WP_UnitTestCase;
+
+/**
+ * Currency Test
+ *
+ * @author Remco Tolsma
+ * @version 1.2.2
+ * @since   1.0.0
+ */
+class CurrencyTest extends WP_UnitTestCase {
+	/**
+	 * Test Euro.
+	 */
+	public function test_euro() {
+		$currency = Currencies::get_currency( 'EUR' );
+
+		$this->assertSame( '978', $currency->get_numeric_code() );
+		$this->assertSame( 'Euro', $currency->get_name() );
+		$this->assertJsonStringEqualsJsonString( '"EUR"', \wp_json_encode( $currency ) );
+	}
+
+	/**
+	 * Test invalid alphabetic code.
+	 */
+	public function test_invalid_alphabetic_code() {
+		$alphabetic_code = 'ABCDEF';
+
+		$this->expectException( \InvalidArgumentException::class );
+
+		$currency = new Currency( 'ABCDEF' );
+	}
+}
